a1da5e01cd0cdff3f9e9d8c4a7b98f5f98a8b262
real-poker <63912990+real-poker@users.noreply.github.com>
2020-05-01 23:11:56 -0400

    Added OptionMenu

--- RNGclick.py
+++ RNGclick.py
@@ -1,29 +1,88 @@
-from tkinter import Tk, Label, StringVar
+import tkinter as tk
 import random
 
-def gen_rand(event):      
-    randvar = random.randint(1,100)  
+colorSwitch = True
+lowLim = 1
+hiLim = 100
+def gen_rand(event):  
+    global colorSwitch
+    global lowLim, hiLim
+    randvar = random.randint(lowLim,hiLim)  
     tout.set(randvar) 
     
-    if randvar < 25:
-        label.config(fg='#FF3333')
-    elif randvar < 50:
-        label.config(fg='#FFAC33')
-    elif randvar < 75:
-        label.config(fg='#FFFF33')
+    if colorSwitch:
+        if lowLim == 0:
+            if randvar < 25:
+                label.config(fg='#FF3333')
+            elif randvar < 50:
+                label.config(fg='#FFAC33')
+            elif randvar < 75:
+                label.config(fg='#FFFF33')
+            else:
+                label.config(fg='#33FF39') 
+        else:
+            if randvar < 26:
+                label.config(fg='#FF3333')
+            elif randvar < 51:
+                label.config(fg='#FFAC33')
+            elif randvar < 76:
+                label.config(fg='#FFFF33')
+            else:
+                label.config(fg='#33FF39') 
     else:
-        label.config(fg='#33FF39') 
-
+        if lowLim == 0:
+            if randvar < 25:
+                label.config(fg='#33FF39')
+            elif randvar < 50:
+                label.config(fg='#FFFF33')
+            elif randvar < 75:
+                label.config(fg='#FFAC33')
+            else:
+                label.config(fg='#FF3333') 
+        else:
+            if randvar < 26:
+                label.config(fg='#33FF39')
+            elif randvar < 51:
+                label.config(fg='#FFFF33')
+            elif randvar < 76:
+                label.config(fg='#FFAC33')
+            else:
+                label.config(fg='#FF3333')   
+                             
 def clear_rand(event):
 	tout.set("")
- 
-root = Tk()
+
+def cSwitch():
+    global colorSwitch
+    colorSwitch = not(colorSwitch)
+    
+def rng99():
+    global lowLim, hiLim
+    lowLim = 0
+    hiLim = 99
+    
+def rng100():
+    global lowLim, hiLim
+    lowLim = 1
+    hiLim = 100  
+    
+root = tk.Tk()
+menubar = tk.Menu(root)
+filemenu = tk.Menu(menubar, tearoff=0)
+
+filemenu.add_command(label="Switch Colour Ranking", command=cSwitch)
+filemenu.add_separator()
+filemenu.add_command(label="RNG from 0-99", command=rng99)
+filemenu.add_command(label="RNG from 1-100", command=rng100)
+
+menubar.add_cascade(label="Options", menu=filemenu)
 root.title('RNG')
 root.configure(background='black')
+root.config(menu=menubar)
 root.bind('<Button-1>', gen_rand)
 root.bind('<Button-3>', clear_rand)
-tout = StringVar()
-label = Label(root, textvariable=tout, font=('Consolas', 100), bg='black')
+tout = tk.StringVar()
+label = tk.Label(root, textvariable=tout, font=('Consolas', 100), bg='black')
 label.pack(side="top", fill="both")
 root.geometry("220x140+200+200")
 root.mainloop()
